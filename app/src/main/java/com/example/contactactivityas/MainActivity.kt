7f0b8e5bc2455f682a462ee12f8a98d391335898
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-03-04 15:47:54 +0200

    MyContacts

--- app/src/main/java/com/example/contactactivityas/MainActivity.kt
+++ app/src/main/java/com/example/contactactivityas/MainActivity.kt
@@ -10,6 +10,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        var firstName = "Mikey"
+        var lastName = "williams"
+        var Adress = "7FairmountaRoad"
+        var isMale = true
+        var dayOfBirth = 22
+        var monthOfBirth = 5
+        var yearOfBirth = 2000
+
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
