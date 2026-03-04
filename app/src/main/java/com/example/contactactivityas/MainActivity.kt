b85917e2d0648650220e05ba622e46ade76039fc
franclinbatchamen-lgtm <franclinbatchamen@gmail.com>
2026-03-04 19:12:55 +0200

    MyContacts

--- app/src/main/java/com/example/contactactivityas/MainActivity.kt
+++ app/src/main/java/com/example/contactactivityas/MainActivity.kt
@@ -5,18 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         var firstName = "Mikey"
         var lastName = "williams"
-        var Adress = "7FairmountaRoad"
+        var Adress = "7FairmountRoad"
         var isMale = true
         var dayOfBirth = 22
         var monthOfBirth = 5
         var yearOfBirth = 2000
+
+        println("${firstName} ${lastName}The place in which you reside${Adress}If i am not mistaken " +
+                "your gender is${true} and your born in the ${dayOfBirth} of the ${monthOfBirth}th in" +
+                " ${yearOfBirth}" )
 
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
